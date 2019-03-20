0c95a689de79a5d9f2ea226b5af6abaf98a2ea60
karthik1108 <karthiktech11@gmail.com>
2019-03-20 11:22:16 +0530

    updated 1

--- src/org/cts/Hello.java
+++ src/org/cts/Hello.java
@@ -4,4 +4,12 @@ public class Hello {
 public static void main(String[] args) {
 	System.out.println("hello");
 }
+private void method3() {
+System.out.println("method3");// TODO Auto-generated method stub
+
+}
+private void method4() {
+System.out.println("method4");	// TODO Auto-generated method stub
+
+}
 }
